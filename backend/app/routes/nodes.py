3ae1bb7d5627eaa2e8377cdd8d8789d2d97653fa
Alexander Blatchford <alexander.blatchford@gmail.com>
2025-08-22 19:18:46 +1000

    Starting from scratch -Alex

--- backend/app/routes/nodes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-from db.db_manager import get_visualization_data
-
-nodes_bp = Blueprint("nodes", __name__)
-
-@nodes_bp.route("/nodes", methods=["GET"])
-def get_node_data():
-    return jsonify(get_visualization_data())
